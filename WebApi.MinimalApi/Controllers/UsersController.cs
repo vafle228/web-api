5cab2de176b4fb53e59d06cbdf52501a7b73f9a4
~ Vafle ~ <dimasmyslaev228@gmail.com>
2025-04-14 15:25:03 +0500

    СРАТЬ

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,27 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
 
 namespace WebApi.MinimalApi.Controllers;
 
-[Route("api/[controller]")]
 [ApiController]
-public class UsersController : Controller
+[Route("api/[controller]")]
+public class UsersController(IUserRepository userRepository, IMapper mapper) : Controller
 {
-    // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-    public UsersController(IUserRepository userRepository)
-    {
-    }
-
-    [HttpGet("{userId}")]
+    [HttpGet("{userId}", Name = nameof(GetUserById))]
+    [Produces("application/json", "application/xml")]
     public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
     {
-        throw new NotImplementedException();
+        var user = userRepository.FindById(userId);
+        return user is null ? NotFound() : Ok(mapper.Map<UserDto>(user));
     }
 
     [HttpPost]
-    public IActionResult CreateUser([FromBody] object user)
+    [Produces("application/json", "application/xml")]
+    public IActionResult CreateUser([FromBody] UserCreateDto? user)
     {
-        throw new NotImplementedException();
+        if (user is null) 
+            return BadRequest();
+        
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+        
+        var entity = userRepository.Insert(mapper.Map<UserEntity>(user));
+        return CreatedAtRoute(nameof(GetUserById), new { userId = entity.Id }, entity.Id);
+    }
+
+    [HttpPut("{userId}")]
+    [Produces("application/json", "application/xml")]
+    public IActionResult UpdateUser([FromRoute] Guid userId, [FromBody] UserUpdateDto? user)
+    {
+        if (user is null || userId == Guid.Empty)
+            return BadRequest();
+        
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+            
+        var entity = mapper.Map(user, new UserEntity(userId)); 
+        userRepository.UpdateOrInsert(entity, out var created);
+        return created ? CreatedAtRoute(nameof(GetUserById), new { userId = entity.Id }, entity.Id) : NoContent();
     }
 }
--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,10 +1,38 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using WebApi.MinimalApi.Domain;
+using WebApi.MinimalApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.WebHost.UseUrls("http://localhost:5000");
-builder.Services.AddControllers()
+
+builder.Services.AddControllers(options =>
+    {
+        options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+        options.ReturnHttpNotAcceptable = true;
+        options.RespectBrowserAcceptHeader = true;
+    })
     .ConfigureApiBehaviorOptions(options => {
         options.SuppressModelStateInvalidFilter = true;
         options.SuppressMapClientErrors = true;
+    })
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Populate;
     });
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.CreateMap<UserEntity, UserDto>()
+        .ForMember(dest => dest.FullName, opt =>
+            opt.MapFrom(src => $"{src.FirstName} {src.LastName}"));
+    cfg.CreateMap<UserCreateDto, UserEntity>();
+    cfg.CreateMap<UserUpdateDto, UserEntity>();
+}, Array.Empty<Assembly>());
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
 
 var app = builder.Build();
 
